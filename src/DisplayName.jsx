bd373ef090b9747d061f1b9f1cf567b98e9a685e
Roopa S <roopa31113@gmail.com>
2024-05-08 13:49:50 +0530

    Logic implemented

---
+++ src/DisplayName.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+
+export default function DisplayName() {
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [fullName, setFullName] = useState("");
+
+  const handleFirstName = (event) => {
+    setFirstName(event.target.value);
+  };
+
+  const handleLastName = (event) => {
+    setLastName(event.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFullName(`${firstName} ${lastName}`);
+  };
+
+  return (
+    <div>
+      <h1>Full Name Display</h1>
+      <form onSubmit={handleSubmit}>
+        <h3>
+          First Name:
+          <input
+            type="text"
+            name="firstName"
+            value={firstName}
+            onChange={handleFirstName}
+            required
+          />
+        </h3>
+        <h3>
+          Last Name:
+          <input
+            type="text"
+            name="lastName"
+            value={lastName}
+            onChange={handleLastName}
+            required
+          />
+        </h3>
+        <button type="submit">Submit</button>
+      </form>
+      <h3>Full Name: {fullName}</h3>
+    </div>
+  );
+}
